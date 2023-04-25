b62b0c1f280ac876ce2e6c0d4bd19d71fcc79dd0
NiKo <53017296+voiceflow-gallagan@users.noreply.github.com>
2023-04-25 17:37:31 +0200

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -1,12 +1,13 @@
 { pkgs }: {
     deps = [
     	pkgs.nodejs-18_x
-    	pkgs.yarn
-    	pkgs.python311
-    	pkgs.chromedriver
     	pkgs.chromium
     	pkgs.glib
     	pkgs.nss
     	pkgs.fontconfig
+        pkgs.python311
+  ];
+  nativeBuildInputs = [
+    pkgs.chromedriver
   ];
 }
