ee42613b91cb1407c3755a5df3887a9457538fe5
NiKo <53017296+voiceflow-gallagan@users.noreply.github.com>
2023-04-25 15:20:43 +0200

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -1,9 +1,13 @@
 { pkgs }: {
 	deps = [
 		pkgs.nodejs-18_x
+		pkgs.glib
+		pkgs.chromedriver
+    pkgs.chromium
     pkgs.nodePackages.typescript-language-server
     pkgs.yarn
     pkgs.replitPackages.jest
 	  pkgs.python311
+	
   ];
 }
